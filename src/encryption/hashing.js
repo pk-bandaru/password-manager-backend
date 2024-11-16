8daedfb431526c20e56cd89fc2f80bf5c050bfd5
Pavan Kalyan Bandaru <bpavankalyan97@gmail.com>
2024-11-16 10:04:08 +0530

    Encryption Module: Hashing

---
+++ src/encryption/hashing.js
@@ -0,0 +1,45 @@
+const crypto = require('crypto');
+const ALGORITHMS = require('./constants');
+const hashKeys = require('../../config/hash-keys.json');
+
+const hashifyCredential = (credential, algorithm, secretKeys) => {
+    const {salt, pepper} = secretKeys;
+    const hashedCredentials = crypto.hash(algorithm, `${salt}${credential}${pepper}`, 'hex');
+    return hashedCredentials;
+}
+
+const hashifyLoginPassword = password => hashifyCredential(
+    password,
+    ALGORITHMS.PASSWORD_HASH_ALGO,
+    hashKeys.loginPassword
+);
+
+const hashifyDefaultPasscode = defaultPasscode => hashifyCredential(
+    defaultPasscode,
+    ALGORITHMS.PASSCODE_HASH_ALGO,
+    hashKeys.defaultPasscode
+);
+
+const hashhifyPrivatePasscode = privatePasscode => hashifyCredential(
+    privatePasscode,
+    ALGORITHMS.PASSCODE_HASH_ALGO,
+    hashKeys.privatePasscode
+);
+
+const verifyLoginPassword = (loginPassword, authenticLoginPassword) => 
+    hashifyLoginPassword(loginPassword) === authenticLoginPassword;
+
+const verifyDefaultPasscode = (defaultPasscode, authenticDefaultPasscode) =>
+    hashifyDefaultPasscode(defaultPasscode) === authenticDefaultPasscode;
+
+const verifyPrivatePasscode = (privatePasscode, authenticPrivatePasscode) => 
+    hashhifyPrivatePasscode(privatePasscode) === authenticPrivatePasscode
+
+module.exports = {
+    hashifyLoginPassword,
+    hashifyDefaultPasscode,
+    hashhifyPrivatePasscode,
+    verifyLoginPassword,
+    verifyDefaultPasscode,
+    verifyPrivatePasscode
+}
