e7153a2912201979d4799aafccb3278a1563db20
Pavan Kalyan Bandaru <bpavankalyan97@gmail.com>
2024-11-18 14:38:42 +0530

    Added Login route

--- src/v1/handler/auth.js
+++ src/v1/handler/auth.js
@@ -1,20 +1,20 @@
 //V1 Authentication Handler
 const dbo = require('../../db/handler/auth');
 const algo = require('../../encryption/hashing');
-const validateNewUserData = require('../../validation/new-user');
+const {getAuthToken, verifyAuthToken} = require('../../token');
+const {validateNewUserData, validateLoginCredentials} = require('../../validation/user');
 
 // Default Error Response
-const sendServerResponse = (response, statusCode, message) => {
+const sendServerResponse = (response, statusCode, data) => {
     response.status(statusCode);
-    response.send(message);
+    response.send(data);
 }
 
-const errorResponse = (response, errorMessage) => sendServerResponse(response, 400, errorMessage);
-const successResponse = (response, successMessage) => sendServerResponse(response, 200, successMessage);
+const errorResponse = (response, errorMessage) => sendServerResponse(response, 400, {message: errorMessage});
 const internalServerErrorResponse = (response) => sendServerResponse(response, 500, 'Internal Server Error');
 
-// POST API: /register
-module.exports.registerNewUser = async (request, response) => {
+// POST API: /register Validation
+module.exports.validateRegistration = async (request, response, next) => {
     const user = request.body;
     const userReport = validateNewUserData(user);
 
@@ -33,6 +33,13 @@ module.exports.registerNewUser = async (request, response) => {
         const errorMessage = `Username '${user.username}' Already Taken`;
         return errorResponse(response, errorMessage);
     }
+    next();
+}
+
+// POST API: /register
+module.exports.registerNewUser = async (request, response) => {
+    const user = request.body;
+    const {db} = request.app.locals;
 
     user.password = algo.hashifyLoginPassword(user.password);
     user.passcode = algo.hashifyDefaultPasscode(user.passcode);
@@ -42,5 +49,54 @@ module.exports.registerNewUser = async (request, response) => {
     if(!insertResponse.isExecutionSuccessful){
         return internalServerErrorResponse(response);
     }
-    sendServerResponse(response, 201, 'User Registration Successful');
+    sendServerResponse(response, 201, {message: 'User Registration Successful'});
+}
+
+module.exports.validateLogin = async (request, response, next) => {
+    const credentials = request.body;
+    const credsReport = validateLoginCredentials(credentials);
+
+    if(!credsReport.isValid){
+        return errorResponse(response, credsReport.message);
+    }
+
+    const {db} = request.app.locals;
+    let dbResponse = await dbo.getUserId(db, credentials.username);
+
+    if(!dbResponse.isExecutionSuccessful){
+        return internalServerErrorResponse(response);
+    }
+
+    if(dbResponse.data.length === 0){
+        return errorResponse(response, 'Invalid Username');
+    }
+
+    const {userId} = dbResponse.data[0];
+    request.body.userId = userId;
+    next();
+}
+
+module.exports.userLogin = async (request, response) => {
+    const {userId, password} = request.body;
+    const {db} = request.app.locals;
+
+    const dbResponse = await dbo.getLoginPassword(db, userId);
+
+    if(!dbResponse.isExecutionSuccessful){
+        return internalServerErrorResponse(response);
+    }
+
+    const {hashedPassword} = dbResponse.data[0];
+    const isValidPassword = algo.verifyLoginPassword(password, hashedPassword);
+
+    if(!isValidPassword){
+        return errorResponse(response, 'Incorrect Password');
+    }
+
+    const jwtToken = getAuthToken(userId);
+    const responseData = {
+        message: 'Login Successful',
+        jwtToken
+    };
+    sendServerResponse(response, 200, responseData);
 }
--- src/v1/router.js
+++ src/v1/router.js
@@ -5,6 +5,7 @@ const authHandler = require('./handler/auth');
 const router = express.Router();
 
 router.get('/', (request, response) => {response.send('V1 Module: Hello User!')});
-router.post('/register', authHandler.registerNewUser);
+router.post('/register', authHandler.validateRegistration, authHandler.registerNewUser);
+router.post('/login', authHandler.validateLogin, authHandler.userLogin);
 
 module.exports = router;
