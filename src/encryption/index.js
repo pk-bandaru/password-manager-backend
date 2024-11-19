a024e3e44d28d86ca50d5312579cdf0918620a21
Pavan Kalyan Bandaru <bpavankalyan97@gmail.com>
2024-11-19 15:59:11 +0530

    Added Encryption, Decryption logic

---
+++ src/encryption/index.js
@@ -0,0 +1,96 @@
+const crypto = require('crypto');
+const STANDARDS = require('./standards');
+const encryptKeys = require('../../keys/encrypt.json');
+
+// Generates Secret Key and Initialization Vector
+function getSecretKeyAndIv(secretKeys, passcode)
+{
+    const {prefix, pepper, suffix, vector} = secretKeys;
+    const key = String.prototype.concat(prefix, passcode, pepper, suffix);
+    const iv = String.prototype.concat(vector, passcode);
+    return {key, iv};
+}
+
+// Encryption Logic
+function encryptData(data, algorithm, secretKeys, passcode)
+{
+    const {key, iv} = getSecretKeyAndIv(secretKeys, passcode);
+    console.log(key.length);
+    console.log(iv.length);
+    
+
+    const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), Buffer.from(iv));
+    const encryptedHexCode = cipher.update(data, 'utf8', 'hex') + cipher.final('hex');
+
+    const authTag = cipher.getAuthTag().toString('hex');
+    return authTag +'.'+ encryptedHexCode;
+}
+
+// Decryption Logic
+function decryptData(encryptedString, algorithm, secretKeys, passcode)
+{
+    const {key, iv} = getSecretKeyAndIv(secretKeys, passcode);
+
+    const encryptedArray = encryptedString.split('.');
+    const authTag = encryptedArray[0];
+    const encryptedHexCode = encryptedArray[1];
+
+    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), Buffer.from(iv));
+    decipher.setAuthTag(Buffer.from(authTag, 'hex'));
+
+    const decrypted = decipher.update(encryptedHexCode, 'hex', 'utf8') + decipher.final('utf8');
+    return decrypted;
+}
+
+// Encryption Constructors
+const encryptAccountName = (accountName, passcode) => encryptData(
+    accountName,
+    STANDARDS.AES_128,
+    encryptKeys.accountName,
+    passcode
+);
+
+const encryptUsername = (username, passcode) => encryptData(
+    username,
+    STANDARDS.AES_256,
+    encryptKeys.username,
+    passcode
+);
+
+const encryptPassword = (password, passcode) => encryptData(
+    password,
+    STANDARDS.AES_256,
+    encryptKeys.password,
+    passcode
+);
+
+// Decryption Constructors
+const decryptAccountName = (encryptedString, passcode) => decryptData(
+    encryptedString,
+    STANDARDS.AES_128,
+    encryptKeys.accountName,
+    passcode
+);
+
+const decryptUsername = (username, passcode) => decryptData(
+    username,
+    STANDARDS.AES_256,
+    encryptKeys.username,
+    passcode
+);
+
+const decryptPassword = (encryptedString, passcode) => decryptData(
+    encryptedString,
+    STANDARDS.AES_256,
+    encryptKeys.password,
+    passcode
+);
+
+module.exports = {
+    encryptAccountName,
+    encryptUsername,
+    encryptPassword,
+    decryptAccountName,
+    decryptUsername,
+    decryptPassword
+}
