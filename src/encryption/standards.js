49c6d49d2c05defddd20ad5ec4901309c3ce509e
Pavan Kalyan Bandaru <bpavankalyan97@gmail.com>
2024-11-19 15:56:21 +0530

    Renamed constants as standards

---
+++ src/encryption/standards.js
@@ -0,0 +1,12 @@
+// Encryption Standards
+const STANDARDS = {
+    SHA_256: 'sha3-512',
+    SHA_256: 'sha3-256',
+    AES_256: 'aes-256-gcm',
+    AES_128: 'aes-128-gcm'
+};
+
+// console.log(crypto.getHashes());
+// console.log(crypto.getCiphers());
+
+module.exports = STANDARDS;
