4bda70c7d86dcb576ae42d1d0ff2a6882fd0b54f
Pavan Kalyan Bandaru <bpavankalyan97@gmail.com>
2024-11-22 17:00:13 +0530

    Modified ID column in generic queries

--- src/db/queries/generic.js
+++ src/db/queries/generic.js
@@ -1,12 +1,10 @@
 
 module.exports.GET_CATEGORIES =
-`SELECT	[category_id] AS id,
-		[category_code] AS code,
+`SELECT	[category_code] AS id,
 		[category_name] AS name
 FROM	[Category] WITH(NOLOCK);`;
 
 module.exports.GET_PASSWORD_TYPES = 
-`SELECT	[type_id] AS id,
-		[type_code] AS code,
+`SELECT	[type_code] AS id,
 		[type_name] AS name
 FROM	[Password_Type] WITH(NOLOCK);`;
